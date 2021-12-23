78eee85aa69514ffbadc4382fa561703135d2abf
darohang088 <darohang009988@gmail.com>
2021-12-23 13:28:30 +0700

    api implement

---
+++ api/src/main/java/com/hangdaro/api/APIConnect.java
@@ -0,0 +1,10 @@
+package com.hangdaro.api;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class APIConnect {
+    public  static  void connect(Context context , String value){
+        Toast.makeText(context, value, Toast.LENGTH_SHORT).show();
+    }
+}
